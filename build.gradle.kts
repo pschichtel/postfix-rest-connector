e87c99794017a551c66c670e8e7416c989ba6fcd
Phillip Schichtel <phillip@schich.tel>
2024-06-07 00:30:34 +0200

    upgrade gradle and kx.serialization

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ version = "2.0.1-SNAPSHOT"
 
 val ktorVersion = "2.3.11"
 val coroutinesVersion = "1.8.1"
-val serializationVersion = " 1.6.3"
+val serializationVersion = " 1.7.0"
 
 repositories {
     mavenCentral()
